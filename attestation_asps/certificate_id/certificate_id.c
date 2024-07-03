27ec1000031d263a5d56f38d82641c4ced55f9e5
Will Thomas <30wthomas@gmail.com>
2024-07-03 15:59:25 -0500

    Adding the other stub asps that we need

---
+++ attestation_asps/certificate_id/certificate_id.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  // Used for debugging the arguments
+  // Print arguments
+  // for (int i = 0; i < argc; i++)
+  // {
+  //   printf("Arg %d: %s\n", i, argv[i]);
+  // }
+  printf("{ \"PAYLOAD\": { \"RawEv\": [\"616E6F6E6365\"] }, \"SUCCESS\": true }");
+  return 0;
+}
