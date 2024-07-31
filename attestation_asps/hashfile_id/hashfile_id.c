05baa650f16f9f0be16aca6424a5452cc307e1c6
Adam Petz <ampetz@ku.edu>
2024-07-31 15:34:58 -0500

    adding hashfile_id asp

---
+++ attestation_asps/hashfile_id/hashfile_id.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "../../json_headers/Copland.h"
+
+int main(int argc, char **argv)
+{
+  ASPRunRequest req = ASPRunRequest_from_string(argv[1]);
+  const char *preamble = "HashingFile(\0";
+  const char *postamble = ")\0";
+  size_t resp_ev_size = strlen(preamble) + (req.raw_ev == NULL ? 0 : strlen(req.raw_ev->ev_val)) + strlen(postamble);
+  char *resp_ev = (char *)malloc(sizeof(char) * resp_ev_size);
+  strcat(resp_ev, preamble);
+  if (req.raw_ev != NULL)
+  {
+    strcat(resp_ev, req.raw_ev->ev_val);
+  }
+  strcat(resp_ev, postamble);
+  ASPRunResponse resp = {true, build_RawEv_T(resp_ev)};
+  printf("%s", ASPRunResponse_to_string(resp));
+  // printf("{ \"TYPE\": \"RESPONSE\", \"ACTION\": \"ASP_RUN\", \"SUCCESS\": true, \"PAYLOAD\": [\"attesting\"]}");
+  return 0;
+}
