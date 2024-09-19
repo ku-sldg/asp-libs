2340bb48adec18bda46b978f1eb734f77ae176c3
Ed Komp <komp@ittc.ku.edu>
2024-09-19 16:12:54 -0500

    include a template for Rust ASPs,
    and update examples to follow the template

--- attestation_asps/rust_asps/src/bin/r_uptime_id.rs
+++ attestation_asps/rust_asps/src/bin/r_uptime_id.rs
@@ -6,7 +6,7 @@
 
 
 use rust_am_lib::copland::*;
-use data_encoding::BASE64;
+use base64;
 use anyhow::{Result};
 use std::env;
 
@@ -17,47 +17,42 @@ fn body() -> Result<String> {
     let args: Vec <String> = env::args().collect();
 
     if args.len() < 2 {
-        Err(anyhow::anyhow!("ASPRunRequest not supplied as command line argument"))
-    } else {
-
-        let json_request = &args[1];
-
-        // decoding the ASPRunRequest just to confirm
-        // that we are being called as expected, even though
-        // no information in the request is required for execution.
-        let _req: ASPRunRequest = serde_json::from_str(json_request)?;
-
-        // This ASP has no arguments.
-        //    let args_map = req.ASP_ARGS;
-
-        // returns seconds since last boot.
-        let up = System::uptime();
-        let up_b64: String = BASE64.encode(&up.to_string().into_bytes());
-
-        let evidence = RawEv::RawEv(vec![up_b64]);
-
-        let  response = ASPRunResponse { TYPE: "RESPONSE".to_string(),
-                                         ACTION: "ASP_RUN".to_string(),
-                                         SUCCESS: true,
-                                         PAYLOAD:  evidence};
-
-        let response_json = serde_json::to_string(&response)?;
-        Ok (response_json)
+        return Err(anyhow::anyhow!("ASPRunRequest not supplied as command line argument"));
     }
+    let json_request = &args[1];
+
+    // decoding the ASPRunRequest just to confirm
+    // that we are being called as expected, even though
+    // no information in the request is required for execution.
+    let _req: ASPRunRequest = serde_json::from_str(json_request)?;
+
+    // This ASP has no arguments.
+    //    let args_map = req.ASP_ARGS;
+
+    // returns seconds since last boot.
+    let up = System::uptime();
+    let up_b64: String = base64::encode(&up.to_be_bytes());
+
+    let evidence = RawEv::RawEv(vec![up_b64]);
+
+    let  response = successfulASPRunResponse (evidence);
+
+    let response_json = serde_json::to_string(&response)?;
+    Ok (response_json)
 }
+
 fn main() -> () {
 
     let response_json = match body() {
         Ok(resp) => resp,
         Err(_error) => {
-            let  response = ASPRunResponse { TYPE: "RESPONSE".to_string(),
-                                             ACTION: "ASP_RUN".to_string(),
-                                             SUCCESS: false,
-                                             PAYLOAD:  RawEv::RawEv(Vec::new())};
+            let  response = failureASPRunResponse (_error.to_string());
             serde_json::to_string(&response).unwrap_or_else(|error| {panic!("Failed to json.encode failure response: {error:?}");})
         }
     };
 
+    // The ASP output (ASPRunRequest) is written to stdout.
+    // The caller will capture stdout to receive the response from this ASP.
     println!("{response_json}");
     ()
 }
