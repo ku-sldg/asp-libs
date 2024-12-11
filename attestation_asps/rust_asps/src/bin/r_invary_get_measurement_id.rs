de6f5a309ff37b7391acbaf801721faffab8bb6f
Adam Petz <ampetz@ku.edu>
2024-12-11 16:10:08 -0600

    refactoring asp args

--- attestation_asps/rust_asps/src/bin/r_invary_get_measurement_id.rs
+++ attestation_asps/rust_asps/src/bin/r_invary_get_measurement_id.rs
@@ -9,6 +9,7 @@ use std::path::PathBuf;
 use std::str;
 use std::thread;
 use std::time::{Duration, UNIX_EPOCH};
+use anyhow::{Context, Result};
 
 const APPRAISAL_DIR: &'static str = "/var/opt/invary-appraiser/appraisals";
 //    handle.url("https://127.0.0.1:8443/api/measurements/jobs")?;
@@ -25,22 +26,44 @@ pub struct InvaryMeasureCheck {
     pub measured: i64,
 }
 
-use anyhow::Result;
 use rust_am_lib::copland::{self, handle_body};
 
 // function where the work of the ASP is performed.
 // May signal an error which will be handled in main.
-fn body(_ev: copland::EvidenceT, _args: copland::ASP_ARGS) -> Result<copland::EvidenceT> {
-    let measure_job_id = demand_measure("veritas")?;
-    thread::sleep(Duration::new(6, 0));
-    let done = check_job_complete(&measure_job_id)?;
+fn body(_ev: copland::EvidenceT, args: copland::ASP_ARGS) -> Result<copland::EvidenceT> {
 
-    if done {
+    let dynamic_arg_val = args
+        .get("dynamic").
+        context("dynamic argument not provided to ASP, r_invary_get_measurement_id")?;
+
+    let dynamic_arg_val_string : String = (*dynamic_arg_val).clone();
+    let true_val_string : String = "true".to_string();
+    let dynamic_arg_bool: bool = dynamic_arg_val_string.eq(&true_val_string);
+
+
+    if dynamic_arg_bool {
+
+        eprint!("\nRequesting dynamic KIM measurement...\n\n");
+
+        let measure_job_id = demand_measure("veritas")?;
+        thread::sleep(Duration::new(6, 0));
+        let done = check_job_complete(&measure_job_id)?;
+
+        if done {
+            eprint!("Reading latest KIM appraisal from directory: {}\n", APPRAISAL_DIR);
+            let path = newest_file_in_dir(APPRAISAL_DIR)?;
+            let bytes = std::fs::read(path)?; // Vec<u8>
+            Ok(vec![bytes])
+        } else {
+            Err(anyhow::anyhow!("Measurement did not complete."))
+        }
+
+    }
+    else {
+        eprint!("\nReading latest KIM appraisal from directory: {}\n\n", APPRAISAL_DIR);
         let path = newest_file_in_dir(APPRAISAL_DIR)?;
         let bytes = std::fs::read(path)?; // Vec<u8>
         Ok(vec![bytes])
-    } else {
-        Err(anyhow::anyhow!("Measurement did not complete."))
     }
 }
 
