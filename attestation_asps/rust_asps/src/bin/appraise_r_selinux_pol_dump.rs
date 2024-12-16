260503d93174ccfd43b00a4080821fbe8ed6b82d
Will Thomas <30wthomas@gmail.com>
2024-12-16 14:04:07 -0600

    selinux policy asps

---
+++ attestation_asps/rust_asps/src/bin/appraise_r_selinux_pol_dump.rs
@@ -0,0 +1,35 @@
+// Common Packages
+use anyhow::Result;
+use rust_am_lib::copland::{self, handle_body};
+
+// function where the work of the ASP is performed.
+// May signal an error which will be handled in main.
+fn body(ev: copland::EvidenceT, _args: copland::ASP_ARGS) -> Result<copland::EvidenceT> {
+    let pol_hash = ev.first().unwrap();
+
+    // let policy_name = _args.get("policy_name").unwrap();
+    let policy_name = "demo_pipeline";
+
+    let golden_policy_path = "demo_pipeline_golden.cil";
+
+    eprint!("Attempting to read from file: {}\n", golden_policy_path);
+
+    let golden_bytes = std::fs::read(golden_policy_path)?; // Vec<u8>
+
+    let out_contents: String = match golden_bytes.eq(pol_hash) {
+        true => "PASSED".to_string(),
+        false => "FAILED".to_string(),
+    };
+
+    Ok(vec![out_contents.as_bytes().to_vec()])
+}
+
+// Main simply invokes the body() function above,
+// and checks for Err Result.
+// If it detects an Err Result, this ASP will return
+// an ASPRunResponse with SUCCESS = false, o/w uses
+// ASPRunResponse returned from body()
+
+fn main() {
+    handle_body(body);
+}
---
+++ attestation_asps/rust_asps/src/bin/r_selinux_pol_dump.rs
@@ -0,0 +1,35 @@
+// Common Packages
+use anyhow::Result;
+use rust_am_lib::copland::{self, handle_body};
+use sha2::{Digest, Sha256};
+
+// function where the work of the ASP is performed.
+// May signal an error which will be handled in main.
+fn body(ev: copland::EvidenceT, _args: copland::ASP_ARGS) -> Result<copland::EvidenceT> {
+    // let policy_name = _args.get("policy_name").unwrap();
+    let policy_name = "demo_pipeline";
+
+    // Execute the shell command to dump the selinux policy
+    let output = std::process::Command::new("semodule --cil --extract {policy_name}");
+
+    // This will place the output in a file named after the policy in the current directory
+    let filename = format! {"{policy_name}.cil"};
+
+    eprint!("Attempting to read from file: {}\n", filename);
+
+    let bytes = std::fs::read(filename.clone())?; // Vec<u8>
+    std::fs::remove_file(filename)?;
+
+    let hash = Sha256::digest(&bytes);
+    Ok(vec![hash.to_vec()])
+}
+
+// Main simply invokes the body() function above,
+// and checks for Err Result.
+// If it detects an Err Result, this ASP will return
+// an ASPRunResponse with SUCCESS = false, o/w uses
+// ASPRunResponse returned from body()
+
+fn main() {
+    handle_body(body);
+}
