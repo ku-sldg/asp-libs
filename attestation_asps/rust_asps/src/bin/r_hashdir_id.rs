ad1e625c255127ce61cdbb18384725190c1d93b3
Adam Petz <ampetz@ku.edu>
2025-01-03 16:43:14 -0600

    adding initial hashdir ASP (not much testing)

---
+++ attestation_asps/rust_asps/src/bin/r_hashdir_id.rs
@@ -0,0 +1,80 @@
+// TEMPLATE.txt
+// General structure for ASP's written in rust
+
+use anyhow::{Context, Result};
+use rust_am_lib::copland::{self, handle_body};
+
+// Packages required to perform specific ASP action.
+// e.g.
+use sha2::{Digest, Sha256};
+use std::{fs, io};
+//use lexical_sort::{StringSort, natural_lexical_cmp};
+
+// function where the work of the ASP is performed.
+// May signal an error which will be handled in main.
+fn body(_ev: copland::EvidenceT, args: copland::ASP_ARGS) -> Result<copland::EvidenceT> {
+    // Code for specific for this ASP.
+    // This example computes the HASH of the file named in an argument for the ASP.
+    // May return an Err Result, which will be captured in main.
+    let dirname = args
+        .get("dirpath")
+        .context("dirpath argument not provided to ASP, hashdir_id")?;
+
+
+    let env_var_key = "DEMO_ROOT";
+    let env_var_string = match std::env::var(env_var_key) {
+        Ok(val) => val,
+        Err(_e) => {
+            panic!("Did not set environment variable DEMO_ROOT")
+        }
+    };
+
+    let dirname_string = (*dirname).clone();
+    let dirname_full = format! {"{env_var_string}{dirname_string}"};
+
+    eprint!("Attempting to read from direcory: {}\n", dirname_full);
+
+    let mut entries = fs::read_dir(&dirname_full)?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+
+    entries.sort();  /* string_sort_unstable(natural_lexical_cmp); */
+    
+    let mut comp_hash: Vec<u8> = Vec::new();
+
+    for entry in entries {
+        // let dir = entry?;
+
+        let bytes = std::fs::read(&entry)?;
+        comp_hash.extend_from_slice(&bytes);
+        //let v = entry.to_owned();
+        eprintln!("{:?}", entry);
+    }
+
+
+    /*
+        let ev_flattened: Vec<u8> = ev.into_iter().flatten().collect();
+
+    let hash = Sha256::digest(&ev_flattened);
+    */
+
+
+    
+    //let bytes = std::fs::read(dirname_full)?; // Vec<u8>
+
+    let hash = Sha256::digest(&comp_hash);
+    
+
+    Ok(vec![hash.to_vec()])
+}
+
+// Main simply invokes the body() function above,
+// and checks for Err Result.
+// If it detects an Err Result, this ASP will return
+// an ASPRunResponse with SUCCESS = false, o/w uses
+// ASPRunResponse returned from body()
+
+fn main() {
+    handle_body(body);
+}
