2b6a2d138d36fc6ddf37dc70392e736a6538b4dd
Adam Petz <ampetz@ku.edu>
2025-02-26 10:56:06 -0600

    adding attack/repair scripts for cds rewrite img

---
+++ attacks/attack_rewrite_img.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MOD_SCRIPT=./mod_userspace_file.sh
+
+ATTACKS_PATH=$ASP_BIN/../attacks
+
+GOOD_PATH=$ATTACKS_PATH/targ_files/rewrite_one_good
+BAD_PATH=$ATTACKS_PATH/targ_files/rewrite_one_bad
+TARG_PATH=$DEMO_ROOT/installed_dir/bin/rewrite_one
+
+$MOD_SCRIPT -g  $GOOD_PATH \
+            -b  $BAD_PATH  \
+            -t  $TARG_PATH \
+            -a
