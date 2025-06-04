28e7151821786adc88c09b9dec46c86be90b2be4
Will Thomas <30wthomas@gmail.com>
2025-06-04 17:28:06 -0500

    Adding some more debug prints

--- executables/sig/src/main.rs
+++ executables/sig/src/main.rs
@@ -1,19 +1,26 @@
 // Common Packages
 use anyhow::Result;
-use rust_am_lib::copland::{self, handle_body};
+use rust_am_lib::{
+    copland::{self, handle_body},
+    debug_print,
+};
 
 // function where the work of the ASP is performed.
 // May signal an error which will be handled in main.
 fn body(ev: copland::ASP_RawEv, _args: copland::ASP_ARGS) -> Result<copland::ASP_RawEv> {
+    debug_print!("Starting signature ASP execution\n");
     let ev_flattened: Vec<u8> = ev.into_iter().flatten().collect();
+    debug_print!("Flattened evidence to {} bytes\n", ev_flattened.len());
+
     // Use openssl to sign the input message
     let key = include_bytes!("../../../common_files/unsecure_priv_key_dont_use.pem");
     let pkey = openssl::pkey::PKey::private_key_from_pem(key)?;
     let mut signer = openssl::sign::Signer::new(openssl::hash::MessageDigest::sha256(), &pkey)?;
     signer.update(&ev_flattened)?;
     let signature = signer.sign_to_vec()?;
-    let bytes = signature;
-    Ok(vec![bytes])
+    debug_print!("Generated signature of {} bytes\n", signature.len());
+
+    Ok(vec![signature])
 }
 
 // Main simply invokes the body() function above,
