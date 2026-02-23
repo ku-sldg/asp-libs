12976203a4ce615b3f9a6c7a50e13c5958e51717
Adam Petz <ampetz@ku.edu>
2026-02-22 23:52:46 -0500

    more robust verus ASPs

--- executables/run_command_verus_appr/src/main.rs
+++ executables/run_command_verus_appr/src/main.rs
@@ -12,23 +12,43 @@ use serde_json::Value;
 fn body(ev: copland::ASP_RawEv, _args: copland::ASP_ARGS) -> Result<Result<()>> {
     // Suppose the file contents are to be extracted from evidence...
 
+    //panic!("Got to run_command_verus_appr");
     let evidence_in = ev.first().context("No file evidence found")?;
 
-    let appraisal_response: Value = serde_json::from_slice(&evidence_in)?;
+    let appraisal_response: std::result::Result<Value, serde_json::Error>= serde_json::from_slice(&evidence_in);
 
-    let appraisal_response_string = appraisal_response["verification-results"]["errors"].as_number();
+    let appraisal_response_decoded: serde_json::Value = {
+        match appraisal_response {
+            Ok(v) => {v}
+            Err(_) => {Value::Null}
 
-    let num_errors = appraisal_response_string.unwrap();
+        }
+    };
 
-    let num_errors_int = num_errors.as_i64().unwrap();
+    //panic!("Got past appraisal_response");
 
-    let app_resp_bool = num_errors_int == 0;
+    let appraisal_response_string = appraisal_response_decoded["verification-results"]["errors"].as_number();
 
-    match app_resp_bool {
-        true => Ok(Ok(())),
-        false => Ok(Err(anyhow::anyhow!("Appraisal was not successful"))),
+    match appraisal_response_string {
+        None => {
+            //panic!("hihihi");
+            Ok(Err(anyhow::anyhow!("Could not parse JSON response from running verus executable")))
+        }
+        Some(num_errors) => {
+
+            let num_errors_int = num_errors.as_i64().unwrap();
+            let app_resp_bool = num_errors_int == 0;
+
+            match app_resp_bool {
+                true => Ok(Ok(())),
+                false => Ok(Err(anyhow::anyhow!("Appraisal was not successful"))),
+            }
+
+        }
+
     }
 }
+    //let num_errors = appraisal_response_string.unwrap();
 
 // Main simply invokes the body() function above,
 // and checks for Err Result.
