511da721bea0834ffaebbacffb5c8599b7b7d50e
Adam Petz <ampetz@ku.edu>
2026-03-03 11:19:30 -0600

    cleanup before merge

--- external_deps/hamrLib/hamrLib/src/lib.rs
+++ external_deps/hamrLib/hamrLib/src/lib.rs
@@ -1,8 +1,8 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-pub mod extract;
-pub use extract::{get_attestation_report_json, HAMR_AttestationReport, 
+pub mod hamr_helpers;
+pub use hamr_helpers::{get_attestation_report_json, HAMR_AttestationReport, 
     HAMR_ComponentReport, HAMR_ComponentContractReport, 
     HAMR_attestation_report_to_File_Slices, File_Slice,
     HAMR_Slice, HAMR_Pos, relpath_to_abspath, Slices_Map
